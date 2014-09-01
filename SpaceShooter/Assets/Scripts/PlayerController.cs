1859f4cc923e9384aaf34eb80340e3849f2bb067
HugCushionKit <krf@aber.ac.uk>
2014-09-01 17:26:44 +0100

    Adding first two projects
    Created using tutorials and assests from
    http://unity3d.com/learn/tutorials/modules

---
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary{
+
+	public float xMin, xMax, zMin, zMax;
+
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotspawn;
+
+	private float nextFire = 0.0f;
+	public float fireRate;
+
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3 (Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+
+	void Update(){
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+					nextFire = Time.time + fireRate;
+					Instantiate (shot, shotspawn.position, shotspawn.rotation);
+					audio.Play();
+				}
+		}
+}
